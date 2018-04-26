f7debe9f8a7b30680413e406d7341790b21b8f78
胡家宝 <jiabao_hu@corp.netease.com>
2018-04-26 13:33:48 +0800

    git

---
+++ git/pull.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import shelve
+import logging
+import time
+
+my_root = '/Users/hujiabao/workspace_bim'
+for root, dirs, files in os.walk(my_root,True):
+    for dir in dirs:
+        path = os.path.join(root, dir)
+        print(path)
+        for root2,dirs2,files2 in os.walk(path,True):
+            if '.git' in dirs2:
+                os.chdir(path)
+                print(os.getcwd())
+                os.system('git checkout dev')
+                os.system('git pull')
+                break
+    break
+
