3d01fadeb490cf89bcc4dd860173bcddbc16b2a7
胡家宝 <jiabao_hu@corp.netease.com>
2018-09-23 11:24:21 +0800

    temp

--- storewords/fileutil.py
+++ storewords/fileutil.py
@@ -13,7 +13,7 @@ from selenium import webdriver
 browser = webdriver.Firefox(executable_path='/Applications/geckodriver')
 browser.get('http://account.youdao.com/login?service=dict&back_url=http://dict.youdao.com/wordbook/wordlist%3Fkeyfrom%3Dlogin_from_dict2.index')
 editor = browser.find_element_by_id('username')
-editor.send_keys('hujb2035@163.com')
+editor.send_keys('hujb2037@163.com')
 password = browser.find_element_by_id('password')
 #password.send_keys('storewords')
 password.send_keys('baojinta')
@@ -77,19 +77,19 @@ for folderName, subfolders, filenames in os.walk(path):
             if len(line) == 0:
                 continue
 
-            if strLen(line) < 12:
+            if strLen(line) < 15:
                 addword = browser.find_element_by_id('addword')
-                time.sleep(0.4)
+                time.sleep(0.6)
                 addword.click()
-                time.sleep(0.4)
+                time.sleep(0.6)
                 word = browser.find_element_by_css_selector('#word')
-                time.sleep(0.4)
+                time.sleep(0.6)
                 logging.info(line)
                 word.send_keys(line)
                 submit = browser.find_element_by_css_selector('#editwordform  form')
-                time.sleep(5)
+                time.sleep(6)
                 submit.submit()
-                time.sleep(0.5)
+                time.sleep(2)
         file.close()
 
 # try:
